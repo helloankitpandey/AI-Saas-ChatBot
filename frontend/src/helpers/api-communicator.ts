95cf4e9a3d4448064da4277c2168e6df0093e170
akii <158286814+helloankitpandey@users.noreply.github.com>
2024-11-27 04:34:33 +0530

    token

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -2,6 +2,8 @@ import axios from "axios"
 
 export const loginUser = async (email: string, password: string) => {
     const res = await axios.post("/user/login", {email, password});
+    const jwt = res.data;
+    localStorage.setItem("token", jwt);
     if(res.status !== 200 ){
         throw new Error("Unable to login");
     }
